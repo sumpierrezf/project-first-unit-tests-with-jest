cb542a7de61cd1205db7280953559bc5337c37a2
sumpierrezf <viqui09umpierrez@gmail.com>
2022-12-21 21:30:59 +0000

    Project first tests with Jest

---
+++ app.js
@@ -0,0 +1,38 @@
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+// declaramos una funcion con el mismo nombre "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar;
+}
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 106.5;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 0.006;
+    return valueInPound;
+}
+
+// esta es mi función que suma dos números
+const sum = (a, b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7, 3))
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = {
+    sum,
+    fromEuroToDollar,
+    fromDollarToYen,
+    fromYenToPound
+};
